39b4476f7103a8eaeafa59edb64397b130e1bc0e
Collins <mutaic84@gmail.com>
2022-05-05 16:44:18 +0300

    handle displaying specific hog info on tile

--- src/components/HogTile.js
+++ src/components/HogTile.js
@@ -1,12 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
+import HogInfo from "./HogInfo"
 
 function HogTile({hogs}){
+  
+  const [showInfo, setShowInfo] = useState(false)
   console.log(hogs)
+
+  const [shownInfo, showHogInfo] = useState(hogs)
+
+  function handleClick(event) {
+    setShowInfo(!showInfo)
+    const name = event.target.alt
+    const infoShown = hogs.filter((hog) => hog.name === name)
+    showHogInfo(infoShown)
+  }
+
+  const hogInfo = shownInfo.map((hog) => {
+    return (
+      <div key={hog.name}>
+      {showInfo ? <HogInfo hog={hog}/> : null}
+      </div>
+    )
+  })
+
+  
+
   const hogTile = hogs.map((hog) => {
     return (
       <div className="pigTile" key={hog.name}>
-      <h2>hog</h2>
-    </div>
+        <h3>{hog.name}</h3>
+        <img src={hog.image} alt={hog.name} onClick={handleClick} />
+        {hogInfo[0].key == hog.name ? hogInfo : null}
+      </div>
     )
   })
   return (
