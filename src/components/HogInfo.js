50135f68aa6df0e7578b36f16519cb5038746269
Collins <mutaic84@gmail.com>
2022-05-05 16:54:05 +0300

    display all info about a hog

--- src/components/HogInfo.js
+++ src/components/HogInfo.js
@@ -2,7 +2,16 @@ import React from "react";
 
 function HogInfo({hog}) {
   return (
-    <div>{hog.name} Hog information</div>
+    <>
+      <h4 className="hoggyText">{hog.name}'s information</h4>
+      <ul>
+        <li>Name: {hog.name}</li>
+        <li>Specialty: {hog.specialty}</li>
+        <li>Weight: {hog.weight} Kilograms</li>
+        <li>IsGreased: {hog.greased ? "Yes" : "No"}</li>
+        <li>Highest Medal: {hog["highest medal achieved"]}</li>
+      </ul>
+    </>
   )
 }
 
